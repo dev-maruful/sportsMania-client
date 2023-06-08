722261503e671acf7e41e38959d1dcef0c05e294
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-06-08 17:24:07 +0600

    registration page design done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import SocialLogin from "../../components/SocialLogin";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="hero p-10 bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Register Here!</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+          <form className="card-body ">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-lg font-medium">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-lg font-medium">Email</span>
+              </label>
+              <input
+                required
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="flex gap-5">
+              <div className="form-control relative">
+                <label className="label">
+                  <span className="label-text text-lg font-medium">
+                    Password
+                  </span>
+                </label>
+                <input
+                  required
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control relative">
+                <label className="label">
+                  <span className="label-text text-lg font-medium">
+                    Confirm Password
+                  </span>
+                </label>
+                <input
+                  required
+                  type="password"
+                  placeholder="confirm password"
+                  className="input input-bordered"
+                />
+              </div>
+            </div>
+            <p className="text-sm">
+              Already have an account?{" "}
+              <Link to="/login">
+                <span className="link font-medium">Login now</span>
+              </Link>
+            </p>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+            <div>
+              <SocialLogin></SocialLogin>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
