f9e1d615dd87806ec913f97d6412b29eafe548bb
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-06-10 22:06:24 +0600

    popular instructors section added on homepage

---
+++ src/pages/Home/NatureActivities.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionTitle from "../../components/SectionTitle";
+
+const NatureActivities = () => {
+  return (
+    <div>
+      <SectionTitle header="Nature Activities"></SectionTitle>
+    </div>
+  );
+};
+
+export default NatureActivities;
